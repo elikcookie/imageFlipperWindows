b0bb1e42c4fd7a76e0b760a5a661a80b886bce16
Elitsa Koleva <53860330+elikcookie@users.noreply.github.com>
2023-07-14 19:34:19 +0100

    mirror exists...

--- dupdup.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-import time
-
-hori = pyautogui.locateCenterOnScreen("hori_flip.png")
-pyautogui.click(hori)
-time.sleep(3)
-save_copy = pyautogui.locateCenterOnScreen("save_copy.png")
-pyautogui.click(save_copy)
--- flipperWin11.py
+++ flipperWin11.py
@@ -1,7 +1,5 @@
 import os
-import time
-import pyautogui
-from PIL import Image
+from PIL import Image, ImageOps
 
 folder_path = r"C:\Users\eliko\OneDrive\Desktop\toflip"
 flipped_folder_path = r"C:\Users\eliko\OneDrive\Desktop\flipped"
@@ -11,37 +9,9 @@ image_files = [file for file in os.listdir(folder_path) if file.lower().endswith
 for image_file in image_files:
     image_path = os.path.join(folder_path, image_file)
     image = Image.open(image_path)
-    os.startfile(image_path)
-    time.sleep(5)  # Increased delay
-    
-    pyautogui.hotkey('ctrl', 'e')
-    time.sleep(5)  # Increased delay
-    
-    pyautogui.hotkey('win', 'up')
-    time.sleep(5)  # Increased delay
-    
-    pyautogui.press('tab', presses=16)
-    pyautogui.press('enter')
-    time.sleep(3)  # Increased delay
-    
-    pyautogui.press('tab', presses=8)
-    pyautogui.press('enter')
-    time.sleep(3)  # Increased delay
-    
-    pyautogui.press('tab', presses=6)
-    pyautogui.press('enter')
-    time.sleep(3)  # Increased delay
-    
-    pyautogui.typewrite(flipped_folder_path)
-    pyautogui.press('enter')
-    time.sleep(3)  # Increased delay
-    
-    pyautogui.press('tab', presses=9)
-    pyautogui.press('enter')
-    time.sleep(3)  # Increased delay
-    
-    pyautogui.hotkey('alt', 'f4')
-    pyautogui.press('enter')
-    time.sleep(3)  # Increased delay
+
+    image_flip = ImageOps.mirror(image)
+    image_flip.save(flipped_folder_path + "\\" + image_file, quality=95)
+
 
 print("Image flipping completed.")
